66e1bf5e2319e665a275ed888e07a11d1a81f017
dino <sandhraprathap@gmail.com>
2021-12-12 18:38:20 +0530

    product display page

--- lib/home.dart
+++ lib/home.dart
@@ -1,13 +1,172 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:furnito/main.dart';
 
-class home extends StatefulWidget{
+class home extends StatefulWidget {
   @override
-_homeState createState()=>_homeState();
+  _homeState createState() => _homeState();
 }
-class _homeState extends State<home>{
-@override 
-Widget build(BuildContext context){
-  return Scaffold(body: Container(),);
-}
+
+class _homeState extends State<home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(75),
+        child: AppBar(
+          backgroundColor: Color.fromRGBO(3, 73, 73, 1),
+          title: Image.asset("image/Furnito.png"),
+        ),
+      ),
+      body: SingleChildScrollView(child:Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+            children: [SizedBox(height: 20),
+              Container(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => login(),
+                          ),
+                        );
+                      },
+                      child: Container(
+                       height: MediaQuery.of(context).size.width*0.5,
+                        width:  MediaQuery.of(context).size.width*0.5,
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                              fit: BoxFit.fitHeight,
+                              image: AssetImage("image/dine.png")),
+                        ),
+                      ),
+                    ),
+                   
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => login(),
+                          ),
+                        );
+                      },
+                      child: Container(
+                        height: MediaQuery.of(context).size.width*0.5,
+                        width:  MediaQuery.of(context).size.width*0.5,
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            fit: BoxFit.fitHeight,
+                            image: AssetImage("image/tv.png"),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                
+              ),
+              SizedBox(height: 20),
+              Container(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => login(),
+                          ),
+                        );
+                      },
+                      child: Container(
+                       height: MediaQuery.of(context).size.width*0.5,
+                        width:  MediaQuery.of(context).size.width*0.5,
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                              fit: BoxFit.fitHeight,
+                              image: AssetImage("image/bs.png")),
+                        ),
+                      ),
+                    ),
+                   
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => login(),
+                          ),
+                        );
+                      },
+                      child: Container(
+                        height: MediaQuery.of(context).size.width*0.5,
+                        width:  MediaQuery.of(context).size.width*0.5,
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            fit: BoxFit.fitHeight,
+                            image: AssetImage("image/sofa.png"),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 20),
+              Container(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => login(),
+                          ),
+                        );
+                      },
+                      child: Container(
+                       height: MediaQuery.of(context).size.width*0.5,
+                        width:  MediaQuery.of(context).size.width*0.5,
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                              fit: BoxFit.fitHeight,
+                              image: AssetImage("image/ottoman.png")),
+                        ),
+                      ),
+                    ),
+                   
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => login(),
+                          ),
+                        );
+                      },
+                      child: Container(
+                        height: MediaQuery.of(context).size.width*0.5,
+                        width:  MediaQuery.of(context).size.width*0.5,
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            fit: BoxFit.fitHeight,
+                            image: AssetImage("image/bed.png"),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 20),
+            ],
+      ),),
+    );
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:furnito/home.dart';
 
 void main() {
